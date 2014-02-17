f5688512faec51c16342f6ec65285d8bebe7733a
土屋 圭 <kei.tsuchiya86@gmail.com>
2014-02-17 20:56:16 +0900

    Add constant meaning solver type

--- lib/liblinear.rb
+++ lib/liblinear.rb
@@ -8,6 +8,18 @@ require 'liblinear/problem'
 require 'liblinear/version'
 
 module Liblinear
+  L2R_LR = Liblinearswig::L2R_LR
+  L2R_L2LOSS_SVC_DUAL = Liblinearswig::L2R_L2LOSS_SVC_DUAL
+  L2R_L2LOSS_SVC = Liblinearswig::L2R_L2LOSS_SVC
+  L2R_L1LOSS_SVC_DUAL = Liblinearswig::L2R_L1LOSS_SVC_DUAL
+  MCSVM_CS = Liblinearswig::MCSVM_CS
+  L1R_L2LOSS_SVC = Liblinearswig::L1R_L2LOSS_SVC
+  L1R_LR = Liblinearswig::L1R_LR
+  L2R_LR_DUAL = Liblinearswig::L2R_LR_DUAL
+  L2R_L2LOSS_SVR = Liblinearswig::L2R_L2LOSS_SVR
+  L2R_L2LOSS_SVR_DUAL = Liblinearswig::L2R_L2LOSS_SVR_DUAL
+  L2R_L1LOSS_SVR_DUAL = Liblinearswig::L2R_L1LOSS_SVR_DUAL
+
   # @param ruby_array [Array<Integer>]
   # @return [SWIG::TYPE_p_int]
   def new_int_array(ruby_array)
