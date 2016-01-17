82d1a86084a9465e8ffd501c92e0805d48543503
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2016-01-17 06:50:51 +0000

    update Liblinear::Parameter

--- lib/liblinear/parameter.rb
+++ lib/liblinear/parameter.rb
@@ -1,42 +1,83 @@
 module Liblinear
   class Parameter
-    include Liblinear
-    include Liblinearswig
-    attr_accessor :param
-
-    # @param param [Hash]
-    def initialize(param = {})
-      @param = Liblinearswig::Parameter.new
-      self.solver_type = 1
-      self.C = 1
-      self.eps = 0.1
-      self.p = 0.1
-      self.nr_weight = 0
-      self.weight_label = []
-      self.weight = []
-      param.each do |k, v|
-        self.send("#{k}=", v)
+    class << self
+      # @return [Float]
+      def default_epsilon(solver_type)
+        case solver_type
+        when Liblinear::L2R_LR              then
+          0.01
+        when Liblinear::L2R_L2LOSS_SVC_DUAL then
+          0.1
+        when Liblinear::L2R_L2LOSS_SVC      then
+          0.01
+        when Liblinear::L2R_L1LOSS_SVC_DUAL then
+          0.1
+        when Liblinear::MCSVM_CS            then
+          0.1
+        when Liblinear::L1R_L2LOSS_SVC      then
+          0.01
+        when Liblinear::L1R_LR              then
+          0.01
+        when Liblinear::L2R_LR_DUAL         then
+          0.1
+        when Liblinear::L2R_L2LOSS_SVR      then
+          0.001
+        when Liblinear::L2R_L2LOSS_SVR_DUAL then
+          0.1
+        when Liblinear::L2R_L1LOSS_SVR_DUAL then
+          0.1
+        end
       end
     end
 
-    # @param weigt_label [Array <Integer>]
-    def weight_label=(weight_label)
-      free_int_array(@param.weight_label)
-      @param.weight_label = new_int_array(weight_label)
+    # @param parameter [Hash]
+    def initialize(parameter)
+      parameter[:weight_labels] = [] if parameter[:weight_labels].nil?
+      parameter[:weights]       = [] if parameter[:weights].nil?
+
+      @parameter = Liblinearswig::Parameter.new
+      @parameter.solver_type  = parameter[:solver_type] || Liblinear::L2R_L2LOSS_SVC_DUAL
+      @parameter.C            = parameter[:cost] || 1.0
+      @parameter.p            = parameter[:sensitive_loss] || 0.1
+      @parameter.eps          = parameter[:epsilon] || self.class.default_epsilon(@parameter.solver_type)
+      @parameter.nr_weight    = parameter[:weight_labels].size
+      @parameter.weight_label = Liblinear::Array::Integer.new(parameter[:weight_labels]).swig
+      @parameter.weight       = Liblinear::Array::Double.new(parameter[:weights]).swig
     end
 
-    # @param weight [Array <Double>]
-    def weight=(weight)
-      free_double_array(@param.weight)
-      @param.weight = new_double_array(weight)
+    # @return [Liblinearswig::Parameter]
+    def swig
+      @parameter
     end
 
-    def method_missing(m, *args)
-      if m.to_s.index('=')
-        @param.send(m.to_sym, args.first)
-      else
-        @param.send(m.to_sym)
-      end
+    # @return [Integer]
+    def solver_type
+      @parameter.solver_type
+    end
+
+    # @return [Float]
+    def cost
+      @parameter.C
+    end
+
+    # @return [Float]
+    def sensitive_loss
+      @parameter.p
+    end
+
+    # @return [Float]
+    def epsilon
+      @parameter.eps
+    end
+
+    # @return [Array <Integer>]
+    def weight_labels
+      Liblinear::Array::Integer.decode(@parameter.weight_label, @parameter.nr_weight)
+    end
+
+    # @return [Array <Float>]
+    def weights
+      Liblinear::Array::Double.decode(@parameter.weight, @parameter.nr_weight)
     end
   end
 end
