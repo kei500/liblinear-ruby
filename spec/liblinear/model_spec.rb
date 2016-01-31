b78a6c977c127786f08764911db7874e4d8870b8
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2016-01-31 12:55:28 +0000

    update test for Liblinear::Model

--- spec/liblinear/model_spec.rb
+++ spec/liblinear/model_spec.rb
@@ -3,99 +3,56 @@ require 'liblinear'
 
 describe Liblinear::Model do
   before do
-    @prob = Liblinear::Problem.new([1, 2], [[1],[2]])
-    @param_1 = Liblinear::Parameter.new
-    @model_classification = Liblinear::Model.new(@prob, @param_1)
-
-    @param_2 = Liblinear::Parameter.new({ solver_type: Liblinear::L2R_L2LOSS_SVR })
-    @model_regression = Liblinear::Model.new(@prob, @param_2)
+    @problem = Liblinear::Problem.new([1, 2], [[1],[2]])
+    @parameter = Liblinear::Parameter.new()
+    @model = Liblinear::Model.train(@problem, @parameter)
   end
 
-  describe '#initialize' do
-    it 'raise ArgumentError when arg_1 not equal [Liblinear::Problem] or arg_2 not equal [Liblinear::Parameter]' do
-      expect{
-        Liblinear::Model.new(1, 2)
-      }.to raise_error(ArgumentError, 'arguments must be [Liblinear::Problem] and [Liblinear::Parameter]')
-    end
-
-    it 'raise Liblinear::InvalidParameter when parameter is invalid' do
-      param = Liblinear::Parameter.new
-      param.C = -1
-      expect{
-        Liblinear::Model.new(@prob, param)
-      }.to raise_error(Liblinear::InvalidParameter, 'C <= 0')
-    end
-
-    it 'raise ArgumentError when argument is not [String]' do
-      expect{
-        Liblinear::Model.new(1)
-      }.to raise_error(ArgumentError, 'argument must be [String]')
+  describe '#swig' do
+    it 'returns [Liblinearswig::Model]' do
+      expect(@model.swig.class).to eq(Liblinearswig::Model)
     end
   end
 
   describe '#class_size' do
-    it 'returns the number of classes' do
-      expect(@model_classification.class_size).to eq(2)
+    it 'returns class size' do
+      expect(@model.class_size).to eq(2)
     end
   end
 
   describe '#feature_size' do
-    it 'returns the number of features' do
-      expect(@model_classification.feature_size).to eq(1)
+    it 'returns feature size' do
+      expect(@model.feature_size).to eq(1)
+    end
+  end
+
+  describe '#feature_weights' do
+    it 'returns feature weights' do
+      expect(@model.feature_weights.first.class).to eq(Float)
+    end
+  end
+
+  describe '#bias' do
+    it 'returns bias' do
+      expect(@model.bias).to eq(-1)
     end
   end
 
   describe '#labels' do
     it 'returns labels' do
-      expect(@model_classification.labels).to eq([1, 2])
+      expect(@model.labels).to eq([1, 2])
     end
   end
 
-  describe '#predict' do
-    it 'returns predicted class' do
-      expect(@model_classification.predict([1]).class).to eq(Float)
-    end
-  end
-
-  describe '#predict_probability' do
-    it 'returns predict_probability' do
-      expect(@model_classification.predict_probability([1]).class).to eq(Hash)
-    end
-  end
-
-  describe '#predict_values' do
-    it 'returns predict_values' do
-      expect(@model_classification.predict_values([1]).class).to eq(Hash)
-    end
-  end
-
-  describe '#coefficient' do
-    it 'returns a coefficient' do
-      expect(@model_classification.coefficient(1, 0).class).to eq(Float)
-    end
-
-    it 'returns a coefficient' do
-      expect(@model_classification.coefficient(1).class).to eq(Float)
-    end
-
-    it 'returns all coefficients' do
-      expect(@model_classification.coefficient.class).to eq(Array)
-    end
-  end
-
-  describe '#bias' do
-    it 'return a bias' do
-      expect(@model_classification.bias.class).to eq(Float)
+  describe '#probability_model?' do
+    it 'returns false' do
+      expect(@model.probability_model?).to eq(false)
     end
   end
 
   describe '#regression_model?' do
-    it 'returns true' do
-      expect(@model_regression.regression_model?).to eq(true)
-    end
-
     it 'returns false' do
-      expect(@model_classification.regression_model?).to eq (false)
+      expect(@model.regression_model?).to eq(false)
     end
   end
 end
